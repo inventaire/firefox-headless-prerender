6d7b641d58b8e4991b6654fc87703df0c06552c8
maxlath <k@maxlath.eu>
2023-12-08 18:38:27 +0100

    removeScriptTags: prevent crash when no script is found

--- server/lib/format_page.js
+++ server/lib/format_page.js
@@ -12,10 +12,12 @@ function removeNoscriptReload (html) {
 
 // Inspired by https://github.com/prerender/prerender/blob/478fa6d0a5196ea29c88c69e64e72eb5507b6d2c/lib/plugins/removeScriptTags.js
 function removeScriptTags (html) {
-  const matches = html.match(/<script(?:.*?)>(?:[\S\s]*?)<\/script>/gi);
-  for (const match of matches) {
-    if (!match.includes('application/ld+json')) {
-      html = html.replace(match, '');
+  const matches = html.match(/<script(?:.*?)>(?:[\S\s]*?)<\/script>/gi)
+  if (matches) {
+    for (const match of matches) {
+      if (!match.includes('application/ld+json')) {
+        html = html.replace(match, '')
+      }
     }
   }
   return html
