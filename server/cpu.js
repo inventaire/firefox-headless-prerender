157bf5ff8f70b309532c9c3b6a56cb7c428fcf3f
maxlath <k@maxlath.eu>
2024-05-03 11:22:09 +0200

    fixup! kill process when CPU load is too high

--- server/cpu.js
+++ server/cpu.js
@@ -1,4 +1,5 @@
 import { cpus, loadavg } from 'node:os'
+import { blue, red } from 'tiny-chalk'
 
 const cpusCount = cpus().length
 
@@ -11,10 +12,10 @@ function killProcessOnExcessCPU () {
   const lastFiveMinutesAverageLoad = loadavg()[1]
   const load = lastFiveMinutesAverageLoad / cpusCount
   if (load > 10) {
-    console.log('5 min average CPU load is too high: exiting process', load)
+    console.log(red('5 min average CPU load is too high: exiting process'), load)
     process.exit(1)
   } else {
-    console.log('5 min average CPU load checked', load)
+    console.log(blue('5 min average CPU load checked:'), load)
   }
 }
 
