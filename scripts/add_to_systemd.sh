294aa46875b99de00be6177aae1b6938efe237e4
maxlath <k@maxlath.eu>
2024-05-03 11:16:22 +0200

    add_to_systemd script: document extra unit options

--- scripts/add_to_systemd.sh
+++ scripts/add_to_systemd.sh
@@ -2,3 +2,10 @@
 
 # env "PATH=$PATH" allows to resolve 'which node' in sudo mode
 sudo env "PATH=$PATH" ./node_modules/.bin/add-to-systemd --user $USER firefox-headless-prerender "$(which node) $(pwd)/server/server.js"
+
+echo "Can be added manually:
+Nice=19
+LimitNICE=19
+OOMScoreAdjust=500
+RestartSec=30
+"
