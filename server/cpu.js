646e3d36f26b166c2139ec01ec7fe7b4ca2cfb82
maxlath <k@maxlath.eu>
2024-05-03 11:14:14 +0200

    kill process when CPU load is too high
    Letting systemd restart the process after a delay
    This typically happens when a prerendering session gets froozen, for yet unidentified reasons

--- server/cpu.js
+++ server/cpu.js
@@ -1,4 +1,5 @@
 import { cpus, loadavg } from 'node:os'
+import { blue, red } from 'tiny-chalk'
 
 const cpusCount = cpus().length
 
@@ -6,3 +7,16 @@ export function getCPUsAverageLoad () {
   const [ lastMinuteAverageLoad ] = loadavg()
   return lastMinuteAverageLoad / cpusCount
 }
+
+function killProcessOnExcessCPU () {
+  const lastFiveMinutesAverageLoad = loadavg()[1]
+  const load = lastFiveMinutesAverageLoad / cpusCount
+  if (load > 10) {
+    console.log(red('5 min average CPU load is too high: exiting process'), load)
+    process.exit(1)
+  } else {
+    console.log(blue('5 min average CPU load checked:'), load)
+  }
+}
+
+setInterval(killProcessOnExcessCPU, 30_000)
